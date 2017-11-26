bc7511fc47c2c76d17a79a8963ee04029c901e3c
Daniel Whiffing <danielwhiffing@gmail.com>
2017-11-26 10:13:30 -0500

    Refactors UIService

--- src/services/UIService.js
+++ src/services/UIService.js
@@ -1,109 +1,29 @@
 export default class UIService {
   constructor (gameService) {
-    const state = gameService.state
-    const x = state.game.width
-    const y = state.game.height
+    this.state = gameService.state
+    const x = this.state.game.width
+    const y = this.state.game.height
 
-    const graphics = state.game.add.graphics(0, 0)
+    const graphics = this.state.game.add.graphics(0, 0)
     graphics.beginFill(0x222222)
     graphics.drawRect(0, 0, x, 120)
-    graphics.endFill()
+    graphics.drawRect(0, y - 115, x, y)
 
-    graphics.moveTo(0, 110)
     graphics.beginFill(0x888888)
     graphics.drawRect(0, 115, x, 5)
-    graphics.endFill()
-
-    graphics.moveTo(0, y - 115)
-    graphics.beginFill(0x222222)
-    graphics.drawRect(0, y - 115, x, y)
-    graphics.endFill()
-
-    graphics.moveTo(0, y - 110)
-    graphics.beginFill(0x888888)
     graphics.drawRect(0, y - 115, x, 5)
     graphics.endFill()
 
-    const banner = state.add.text(state.world.centerX, 60, 'Dungeon Raid')
+    const banner = this._initText(x / 2, 60, 'Dungeon Raid', '#77BFA3', 40)
     banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
 
-    const gold = state.add.text(70, state.game.height - 40, '0/50')
-    gold.padding.set(10, 16)
-    gold.fontSize = 32
-    gold.fill = '#ffff00'
-    gold.smoothed = false
-    gold.anchor.setTo(0.5)
-
-    const health = state.add.text(
-      state.game.width - 70,
-      state.game.height - 40,
-      '50/50'
-    )
-    health.padding.set(10, 16)
-    health.fontSize = 32
-    health.fill = '#ff0000'
-    health.smoothed = false
-    health.anchor.setTo(0.5)
-
-    const armor = state.add.text(
-      state.game.width / 2,
-      state.game.height - 70,
-      '0/4'
-    )
-    armor.padding.set(10, 16)
-    armor.fontSize = 18
-    armor.fill = '#0000ff'
-    armor.smoothed = false
-    armor.anchor.setTo(0.5)
-
-    const base = state.add.text(
-      state.game.width / 2 - 60,
-      state.game.height - 70,
-      '+5'
-    )
-    base.padding.set(10, 16)
-    base.fontSize = 18
-    base.fill = '#ffffff'
-    base.smoothed = false
-    base.anchor.setTo(0.5)
-
-    const weapon = state.add.text(
-      state.game.width / 2 + 60,
-      state.game.height - 70,
-      '+3'
-    )
-    weapon.padding.set(10, 16)
-    weapon.fontSize = 18
-    weapon.fill = '#ffffff'
-    weapon.smoothed = false
-    weapon.anchor.setTo(0.5)
-
-    const upgrade = state.add.text(
-      state.game.width / 2,
-      state.game.height - 40,
-      '0/100'
-    )
-    upgrade.padding.set(10, 16)
-    upgrade.fontSize = 24
-    upgrade.fill = '#0000ff'
-    upgrade.smoothed = false
-    upgrade.anchor.setTo(0.5)
-
-    const experience = state.add.text(
-      state.game.width / 2,
-      state.game.height - 10,
-      '0/100'
-    )
-    experience.padding.set(10, 16)
-    experience.fontSize = 24
-    experience.fill = '#00ff00'
-    experience.smoothed = false
-    experience.anchor.setTo(0.5)
+    const gold = this._initText(70, y - 40, '0/50', '#ffff00', 32)
+    const health = this._initText(x - 70, y - 40, '50/50', '#ff0000', 32)
+    const armor = this._initText(x / 2, y - 70, '0/4', '#0000ff', 18)
+    const base = this._initText(x / 2 - 60, y - 70, '+5', '#ffffff', 18)
+    const weapon = this._initText(x / 2 + 60, y - 70, '+3', '#ffffff', 18)
+    const upgrade = this._initText(x / 2, y - 40, '0/100', '#0000ff', 24)
+    const experience = this._initText(x / 2, y - 10, '0/100', '#00ff00', 24)
 
     this.texts = { gold, health, armor, base, weapon, upgrade, experience }
   }
@@ -116,5 +36,15 @@ export default class UIService {
     this.texts.experience.text = `${data.experience}/${data.maxExperience}`
     this.texts.weapon.text = `${data.weapon}`
     this.texts.base.text = `${data.base}`
+  }
+
+  _initText (x, y, string, fill, size) {
+    const text = this.state.add.text(x, y, string)
+    text.padding.set(10, 16)
+    text.smoothed = false
+    text.anchor.setTo(0.5)
+    text.fontSize = size
+    text.fill = fill
+    return text
   }
 }
