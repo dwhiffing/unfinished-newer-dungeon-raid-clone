86ac7e2a61889b914a6a65dbfb5e5fe50954de5f
Daniel Whiffing <danielwhiffing@gmail.com>
2017-11-27 09:17:46 -0500

    Refactors match service

--- src/services/MatchService.js
+++ src/services/MatchService.js
@@ -1,55 +1,28 @@
 export default class MatchService {
   constructor (gameService) {
     this.game = window.game
-    this.match = []
-    this.path = []
     this.tileService = gameService.tileService
+    this.path = []
+    this.match = []
   }
 
   selectTile (position) {
-    let last
     const tile = this.tileService.pickTile(position)
-
-    if (!tile) {
+    if (!tile || this._checkForDeselect(tile) || tile.picked) {
       return
     }
 
-    // check for deselect
-    if (this.path.length > 0) {
-      last = this.tileService.tiles[this.path[this.path.length - 1]]
-      const last2 = this.tileService.tiles[this.path[this.path.length - 2]]
-      if (last2 && tile.index === last2.index) {
-        last.unpick()
-        this.path.pop()
-        return
-      }
+    if (this.path.length === 0) {
+      this._highlightMatchingTiles(tile.frame)
+      this._select(tile)
+      return tile
     }
 
-    if (!last || this._isValidMatch(tile, last)) {
-      if (tile.picked) {
-        return
-      }
-      tile.pick()
-      if (!last) {
-        this.path = []
-        this.match = []
-        this._highlightMatchingTiles(tile.frame)
-      }
-      this.path.push(tile.index)
-      return last ? [tile, last] : tile
+    const last = this._getLast(1)
+    if (this._isValidMatch(tile, last)) {
+      this._select(tile)
+      return [tile, last]
     }
-  }
-
-  _highlightMatchingTiles (frame) {
-    this.tileService.tiles.forEach(t => {
-      if (frame === 0 || frame === 1) {
-        if (t.frame > 1) {
-          t.alpha = 0.5
-        }
-      } else if (t.frame !== frame) {
-        t.alpha = 0.5
-      }
-    })
   }
 
   resolveMatch () {
@@ -63,24 +36,54 @@ export default class MatchService {
       this.match.push(tile)
     }
 
-    const match = this.match.concat([])
-    this.clearPath()
-
-    return match
+    return this.clearPath()
   }
 
   clearPath () {
+    const match = this.match.concat([])
     this.path = []
     this.match = []
     this.tileService.tiles.forEach(t => {
       t.alpha = 1
     })
 
-    return this.path
+    return match
   }
 
   getTilesInMatch () {
     return this.path.map(t => this.tileService.tiles[t])
+  }
+
+  _select (tile) {
+    tile.pick()
+    this.path.push(tile.index)
+  }
+
+  _checkForDeselect (tile) {
+    if (this._getLast(2) && tile.index === this._getLast(2).index) {
+      this._getLast(1).unpick()
+      this.path.pop()
+      return true
+    }
+  }
+
+  _getLast (n) {
+    if (this.path.length < n) {
+      return
+    }
+    return this.tileService.tiles[this.path[this.path.length - n]]
+  }
+
+  _highlightMatchingTiles (frame) {
+    this.tileService.tiles.forEach(t => {
+      if (frame === 0 || frame === 1) {
+        if (t.frame > 1) {
+          t.alpha = 0.5
+        }
+      } else if (t.frame !== frame) {
+        t.alpha = 0.5
+      }
+    })
   }
 
   _isValidMatch (tile, last) {
