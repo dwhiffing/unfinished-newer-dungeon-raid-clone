4640f51054bc9221bd7e6b5543fadb8c95a70ca5
Daniel Whiffing <danielwhiffing@gmail.com>
2017-11-29 19:23:00 -0500

    Fixes responsive code once more

--- src/main.js
+++ src/main.js
@@ -10,18 +10,19 @@ import GameOverState from './states/GameOver'
 
 class Game extends Phaser.Game {
   constructor () {
-    window.ratio = 1
-    const width = window.innerWidth * window.ratio
-    const height = window.innerHeight
-
-    super(width, height, Phaser.CANVAS, 'content', null)
-
-    window.gridDim = 6
     const tileSize = 70
+    window.gridDim = 6
+    const naturalGridSize = window.gridDim * tileSize
+    window.ratio = window.innerWidth / naturalGridSize
+
     window.gridSize = window.gridDim * tileSize * window.ratio
     window.tileSize = window.gridSize / window.gridDim
-    window.leftBuffer = window.innerWidth / 2 - window.gridSize / 2 - 1
-    window.topBuffer = (window.innerHeight - window.gridSize) / 2 - 50
+    window.leftBuffer = window.innerWidth / 2 - window.gridSize / 2
+    window.topBuffer = window.innerHeight - window.gridSize - 120
+
+    const width = window.innerWidth
+    const height = window.innerHeight
+    super(width, height, Phaser.CANVAS, 'content', null)
 
     this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
